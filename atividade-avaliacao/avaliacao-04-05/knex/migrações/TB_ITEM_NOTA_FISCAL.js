3a448fd2e56dbaf8c71e272bbc72746c01d661a8
Abraão Lucas <abraaolucassb@gmail.com>
2022-06-22 12:07:37 -0300

    Alterado em 22-06-2022

---
+++ atividade-avaliacao/avaliacao-04-05/knex/migrações/TB_ITEM_NOTA_FISCAL.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex, Promise) {
+    return knex.schema.createTable('TB_ITEM_NOTA_FISCAL', table => {
+        table.increments('id').primary()
+        table.integer('sequencial').notNull().unique()
+        table.integer('quantidade').notNull()
+        table.string('produto').references('TB_PRODUTO.id')
+        table.string('notaFiscal').references('TB_NOTA_FISCAL_VENDA.id')
+    })
+};
+
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('TB_ITEM_NOTA_FISCAL')
+}
